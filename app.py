9dce1ce1841b09778ebc4991455e7979958fda95
arnavpatel82 <arnav.patel82@yahoo.com>
2022-06-07 16:57:14 +0800

    completed 'Menu Option A' task

--- app.py
+++ app.py
@@ -32,15 +32,14 @@ def add_csv():
                     product_in_db.product_price = product_price
                     product_in_db.product_quantity = product_quantity
                     product_in_db.date_updated = date_updated
-                    
-            
+                    print('product already in database, information updated')
             else:
                 product_to_add = Product(product_name=product_name, product_quantity=product_quantity, product_price=product_price, date_updated=date_updated)
                 session.add(product_to_add)
             
             session.commit()
+            
                     
-
 def view():
     available_ids = []
     for product in session.query(Product):
@@ -56,14 +55,60 @@ def view():
     
     print()
     chosen_product = session.query(Product).filter_by(id=id_choice).first()
+    print("------------------") 
     print(f'Name: {chosen_product.product_name}')
     print(f'Quantity: {chosen_product.product_quantity}')
     print(f'Price: ${chosen_product.product_price / 100}')
     print(f"Date Updated: {chosen_product.date_updated.strftime('%m/%d/%Y')}")
+    print("------------------") 
 
 
+def add():
+    product_name = input('Name: ')
+    print()
+    product_in_db = session.query(Product).filter(Product.product_name==product_name).one_or_none()
 
+    product_quantity = input('Quantity: ')
+    print()
+    while not product_quantity.isdigit():
+        print('quantity must be a whole number')
+        product_quantity = input('Quantity: ')
+        print()
+    
+    while True:
+        try:
+            product_price = input('Price: ')
+            print()
+            product_price = '$' + product_price
+            product_price = clean_price(product_price)
+            break
+        except ValueError:
+            print("price must be a number such as: '15' or '15.99'")
 
+    while True:
+        try:
+            date_updated = input('Date Updated: ')
+            print()
+            date_updated = clean_date(date_updated)
+            break
+        except ValueError:
+            print('date updated must be in this format --> MM/DD/YYYY')
+
+    if product_in_db != None:
+        if product_in_db.date_updated < date_updated:
+            product_in_db.product_name = product_name
+            product_in_db.product_price = product_price
+            product_in_db.product_quantity = product_quantity
+            product_in_db.date_updated = date_updated
+            print('product already in database, information updated')
+    else:
+        product_to_add = Product(product_name=product_name, product_quantity=product_quantity, product_price=product_price, date_updated=date_updated)
+        session.add(product_to_add)
+        print('Product added')
+
+    session.commit()
+
+    
 def menu():
     print()
     print("------ MENU ------")        
@@ -82,10 +127,11 @@ def menu():
 
     if choice == 'v':
         view()
+        input('press enter to return to menu')
 
     elif choice == 'a':
-        # add
-        pass
+        add()
+        input('press enter to return to menu')
 
     else:
         # backup
@@ -96,4 +142,5 @@ def menu():
 if __name__ == "__main__":
     # Base.metadata.create_all(engine)
     # add_csv()
-    menu()
+    while True:
+        menu()
